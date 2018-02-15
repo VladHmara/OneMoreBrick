862e7d1aa8e656b8f678a694da2cc01e3d2f6317
Vlad_Hmara <hmara_vlad@mail.ru>
2018-02-15 21:40:43 +0200

    Add arrow and collisions

--- ball.js
+++ ball.js
@@ -1,5 +1,5 @@
-class Ball{
-    constructor(x,y,sx,sy){
+class Ball {
+    constructor(x, y, sx, sy) {
         this.x = x;
         this.y = y;
 
@@ -9,70 +9,136 @@ class Ball{
         this.div = this.drawBall()
         this.moveBall();
     }
-    static moveBalls(sx,sy){// переименовать
-        for (var i = 0; i < Ball.count; i++){
-            if(Ball.firstBall == null)
-                Ball.balls.push(new Ball(Ball.startPointX,Ball.startPointY,sx,sy));
-            else {
-                Ball.firstBall.sx = sx;
-                Ball.firstBall.sy = sy;
-                Ball.balls.push(Ball.firstBall);
-                Ball.firstBall = null;
-            }
-        }
 
+    static runBalls(sx, sy) {
+        var spx = Ball.startPointX;
+        var spy = Ball.startPointY;
+        Ball.locker = true;
+        Ball.arrowHide();
+        var count = Ball.count;
+        Ball.firstBall.sx = sx;
+        Ball.firstBall.sy = sy;
+        Ball.balls.push(Ball.firstBall);
+        Ball.firstBall = null;
+        count--;
+        var interval1 = setInterval(function () {
+            if (count <= 0)
+                clearInterval(interval1);
+            else {
+                Ball.balls.push(new Ball(spx, spy, sx, sy));
+                count--;
+            }
+        }, 1000 / Ball.speed);
+        var interval2 = setInterval(function () {
+            if (count <= 0 && Ball.balls.length == 0) {
+                Box.drawBoxes();
+                Ball.count++;
+                Ball.locker = false;
+                clearInterval(interval2);
+            }
+        }, 1000 / 60);
     }
 
-    drawBall(){
+    drawBall() {
         var d = document.createElement('div');
         d.style.position = 'absolute';
         d.style.left = this.x + 'px';
-        d.style.top = this.y +'px';
-        d.style.width = 2*Ball.r + 'px';
-        d.style.height = 2*Ball.r + 'px';
+        d.style.top = this.y + 'px';
+        d.style.width = 2 * Ball.r + 'px';
+        d.style.height = 2 * Ball.r + 'px';
         d.style.backgroundColor = Ball.color;
         d.style.borderRadius = Ball.r + 'px';
         Ball.parent.appendChild(d);
         return d;
     }
 
-    moveBall(){
+    moveBall() {
         var ins = this;
-        setInterval(function(){
+        var interval = setInterval(function () {
             ins.x += ins.sx * Ball.speed;
             ins.y += ins.sy * Ball.speed;
 
-            ins.div.style.left = ins.x  + 'px';
+            if (ins.x <= 0 || ins.x + 2 * Ball.r >= 698)
+                ins.sx *= -1;
+
+            if (ins.y <= 0)
+                ins.sy *= -1;
+
+            if (ins.y + 2 * Ball.r >= 900) {
+                if (Ball.firstBall == null) {
+                    ins.sx = 0;
+                    ins.sy = 0;
+                    ins.y = 898 - 2 * Ball.r;
+                    Ball.setFirstBall(ins);
+                }
+                else {
+                    Ball.parent.removeChild(ins.div);
+                    clearInterval(interval);
+                }
+                Ball.balls.splice(Ball.balls.indexOf(ins), 1);
+            }
+
+            Box.collisions(ins);
+
+            ins.div.style.left = ins.x + 'px';
             ins.div.style.top = ins.y + 'px';
-        },1000/60)
+        }, 1000 / 60)
     }
 
-    static setFirstBall(ball){
+    static setFirstBall(ball) {
         Ball.firstBall = ball;
         Ball.startPointX = ball.x;
         Ball.startPointY = ball.y;
     }
-    static setParent(parent){
+
+    static setParent(parent) {
         Ball.parent = parent;
         parent.onmousemove = Ball.mouseMove;
         parent.onclick = Ball.mouseClick;
+        parent.onmouseleave = Ball.arrowHide;
+        parent.onmouseenter = Ball.arrowShow;
     }
 
-    static mouseMove(e){
+    static mouseMove(e) {
+        if (!Ball.locker) {
+            Ball.arrowShow();
+            arrow.style.left = Ball.startPointX + Ball.r - 3 + 'px';
+            arrow.style.top = Ball.startPointY + Ball.r + 'px';
 
+            var vx = e.pageX - 8 - (Ball.startPointX + Ball.r);
+            var vy = e.pageY - 8 - (Ball.startPointY + Ball.r);
+            var wv = Math.sqrt(Math.pow(vx, 2) + Math.pow(vy, 2));
+            arrow.style.width = wv + 'px';
+
+            var rad = Math.acos(vx / wv);
+            arrow.style.transform = 'rotate(-' + rad + 'rad)';
+            if (Math.acos(vy / wv) < 1.575) {
+                Ball.arrowHide();
+            }
+        }
     }
-    static mouseClick(e){
-        console.log(e.layerX + ' ' + e.layerY);
-        console.log((Ball.startPointX + Ball.r) + ' ' + (Ball.startPointY + Ball.r));
-        var vx = e.layerX - (Ball.startPointX + Ball.r);
-        var vy = e.layerY - (Ball.startPointY + Ball.r);
-        console.log(vx + ' ' + vy);
-        var sx = vx/(Math.abs(vx)+Math.abs(vy));
-        var sy = vy/(Math.abs(vx)+Math.abs(vy));
-        console.log(sx + ' ' + sy);
-        Ball.moveBalls(sx,sy);
+
+    static arrowHide() {
+        arrow.style.display = 'none';
+    }
+
+    static arrowShow() {
+        if(!Ball.locker)
+            arrow.style.display = 'flex';
+    }
+
+    static mouseClick(e) {
+        if (!Ball.locker) {
+            var vx = e.pageX - 8 - (Ball.startPointX + Ball.r);
+            var vy = e.pageY - 8 - (Ball.startPointY + Ball.r);
+            var sx = vx / (Math.abs(vx) + Math.abs(vy));
+            var sy = vy / (Math.abs(vx) + Math.abs(vy));
+            Ball.runBalls(sx, sy);
+        }
     }
 }
+
+Ball.arrow = null;
 Ball.balls = [];
 Ball.locker = false;
 Ball.parent = null;
@@ -80,6 +146,6 @@ Ball.firstBall = null;
 Ball.startPointX = 678;
 Ball.startPointY = 878;
 Ball.r = 10;
-Ball.speed = 1;
+Ball.speed = 10;
 Ball.color = 'blue';
 Ball.count = 1;
--- box.js
+++ box.js
@@ -1,7 +1,7 @@
 class Box {
     constructor(x, type) {
         this.x = x;
-        this.y = 1;
+        this.y = 0;
         this.type = type;
         this.life = Box.count;
         Box.boxes.push(this);
@@ -12,10 +12,12 @@ class Box {
         for (var i = 0; i < 7; i++) {
             if (Math.random() >= 0.5)
                 if (Math.random() >= 0.5)
-                    new Box(i, Math.round(0), 1);
+                    new Box(i, 0);
                 else
-                    new Box(i, Math.round(Math.random() * 3 + 1), 1);
+                    new Box(i, Math.round(Math.random() * 3 + 1));
         }
+        Box.moveBoxesDown();
+        Box.count++;
     }
 
     drawBox() {
@@ -59,13 +61,6 @@ class Box {
     static moveBoxesDown() {
         for (var i = Box.boxes.length - 1; i >= 0; i--)
             Box.boxes[i].moveBoxDown();
-        var interval = setInterval(function () {
-            if (!Box.locker) {
-                Box.count++;
-                Box.drawBoxes();
-                clearInterval(interval);
-            }
-        }, 100);
     }
 
     moveBoxDown() {
@@ -74,18 +69,160 @@ class Box {
         var count = 100;
         var ins = this;
         var interval = setInterval(function () {
-            if (count == 0) {
-                Box.locker = false;
+            if (count <= 0)
                 clearInterval(interval);
-            }
             ins.div.style.top = ins.y * 100 - count + 1 + 'px';
-            count-=5;
-        }, 1000/60);
+            count -= 5;
+        }, 1000 / 60);
     }
 
+    static collisions(ball) {
+        var c = 0;
+        for (var i = 0; i < Box.boxes.length; i++) {
+            var box = Box.boxes[i - c];
+            var x = box.x * 100 + 1;
+            var y = box.y * 100 + 1;
+            var vx = ball.x + Ball.r - (x + 48);
+            var vy = ball.y + Ball.r - (y + 48);
+            switch (box.type) {
+                case 0:
+                    if (ball.x < x + 96 &&
+                        ball.x + 2 * Ball.r > x &&
+                        ball.y < y + 96 &&
+                        ball.y + 2 * Ball.r > y) {
+                        if (vx > vy) //верхняя правая
+                            if (Math.abs(vx) < Math.abs(vy)) //верхняя
+                                ball.sy = -1 * Math.abs(ball.sy);
+                            else //правая
+                                ball.sx = Math.abs(ball.sx);
+                        else {  //нижняя левая
+                            if (Math.abs(vx) > Math.abs(vy)) //левая
+                                ball.sx = -1 * Math.abs(ball.sx);
+                            else //нижняя
+                                ball.sy = Math.abs(ball.sy);
+                        }
+                        box.life--;
+                        box.div.innerHTML = box.life;
+                        if (box.life <= 0) {
+                            Box.boxes.splice(i - c, 1);
+                            Box.parent.removeChild(box.div);
+                            c++;
+                        }
+                    }
+                    break;
+                case 1:
+                    if (ball.x < x + 96 &&
+                        ball.y + 2 * Ball.r > y &&
+                        ball.y < ball.x + 2 * Ball.r - x + y) {
+                        if (vx > vy) //верхняя правая
+                            if (Math.abs(vx) < Math.abs(vy)) //верхняя
+                                ball.sy = -1 * Math.abs(ball.sy);
+                            else //правая
+                                ball.sx = Math.abs(ball.sx);
+                        else {  //нижняя левая
+                            var tmp = ball.sx;
+                            ball.sx = ball.sy;
+                            ball.sy = tmp;
+                        }
+                        box.life--;
+                        box.div.innerHTML = box.life;
+                        if (box.life <= 0) {
+                            Box.boxes.splice(i - c, 1);
+                            Box.parent.removeChild(box.div);
+                            c++;
+                        }
+                    }
+                    break;
+                case 2:
+                    if (ball.x < x + 96 &&
+                        ball.y < y + 96 &&
+                        ball.y + 2 * Ball.r > -1 * (ball.x + 2 * Ball.r) + x + y + 96) {
+                        if (vx > vy) //верхняя правая
+                            if (Math.abs(vx) < Math.abs(vy)) { //верхняя
+                                var tmp = ball.sx;
+                                ball.sx = -1 * ball.sy;
+                                ball.sy = -1 * tmp;
+                            }
+                            else //правая
+                                ball.sx = Math.abs(ball.sx);
+                        else {  //нижняя левая
+                            if (Math.abs(vx) > Math.abs(vy)) { //левая
+                                var tmp = ball.sx;
+                                ball.sx = -1 * ball.sy;
+                                ball.sy = -1 * tmp;
+                            }
+                            else //нижняя
+                                ball.sy = Math.abs(ball.sy);
+                        }
+                        box.life--;
+                        box.div.innerHTML = box.life;
+                        if (box.life <= 0) {
+                            Box.boxes.splice(i - c, 1);
+                            Box.parent.removeChild(box.div);
+                            c++;
+                        }
+                    }
+                    break;
+                case 3:
+                    if (ball.x + 2 * Ball.r > x &&
+                        ball.y < y + 96 &&
+                        ball.y + 2 * Ball.r > ball.x - x + y) {
+                        console.log(vx + ' ' + vy);
+                        if (vx > vy) { //верхняя правая
+                            var tmp = ball.sx;
+                            ball.sx = ball.sy;
+                            ball.sy = tmp;
+                        }
+                        else {  //нижняя левая
+                            if (Math.abs(vx) > Math.abs(vy)) //левая
+                                ball.sx = -1 * Math.abs(ball.sx);
+                            else //нижняя
+                                ball.sy = Math.abs(ball.sy);
+                        }
+                        box.life--;
+                        box.div.innerHTML = box.life;
+                        if (box.life <= 0) {
+                            Box.boxes.splice(i - c, 1);
+                            Box.parent.removeChild(box.div);
+                            c++;
+                        }
+                    }
+                    break;
+                case 4:
+                    if (ball.x + 2 * Ball.r > x &&
+                        ball.y + 2 * Ball.r > y &&
+                        ball.y < -1 * ball.x + x + y + 96) {
+                        if (vx > vy) //верхняя правая
+                            if (Math.abs(vx) < Math.abs(vy)) //верхняя
+                                ball.sy = -1 * Math.abs(ball.sy);
+                            else { //правая
+                                var tmp = ball.sx;
+                                ball.sx = -1 * ball.sy;
+                                ball.sy = -1 * tmp;
+                            }
+                        else {  //нижняя левая
+                            if (Math.abs(vx) > Math.abs(vy)) //левая
+                                ball.sx = -1 * Math.abs(ball.sx);
+                            else { //нижняя
+                                var tmp = ball.sx;
+                                ball.sx = -1 * ball.sy;
+                                ball.sy = -1 * tmp;
+                            }
+                        }
+                        box.life--;
+                        box.div.innerHTML = box.life;
+                        if (box.life <= 0) {
+                            Box.boxes.splice(i - c, 1);
+                            Box.parent.removeChild(box.div);
+                            c++;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
 }
 
-Box.locker = false;
 Box.count = 1;
 Box.parent = null;
 Box.boxes = [];
